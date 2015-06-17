a0bb0113436beea96add3e2a92909ff69d8cf0db
Masood Anwer <masood.anwer@aspose.com>
2015-06-17 14:38:31 +0500

    Add unit tests

--- SDKs/Aspose.Words_Cloud_SDK_for_PHP/tests/Aspose/Words/WordsApiTest.php
+++ SDKs/Aspose.Words_Cloud_SDK_for_PHP/tests/Aspose/Words/WordsApiTest.php
@@ -227,5 +227,290 @@ class WordsApiTest extends PHPUnit_Framework_TestCase {
         $result = $this->words->GetSections($name="Test.docx", $storage = null, $folder = null);
         $this->assertEquals(200, $result->Code);
     }
+    
+    public function testPostAppendDocument()
+    {       
+        $appendDocs = array("test_multi_pages.docx");
+        $importFormatsModes = array("KeepSourceFormatting", "UseDestinationStyles");
+
+        $body = '{ "DocumentEntries": [';
+        for ($i = 0; $i < count($appendDocs); $i++) {
+            $body .= '{ "Href": "' . $appendDocs[$i] .
+                    '", "ImportFormatMode": "' . $importFormatsModes[$i] . '" }' .
+                    (($i < (count($appendDocs) - 1)) ? ',' : '');
+        }
+        $body .= '  ] }';
+        
+        $result = $this->words->PostAppendDocument($name="Test.docx", $filename = null, $storage = null, $folder = null, $body);        
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostChangeDocumentProtection()
+    {       
+        $body = array("Password" => "123456", "NewPassword" => "123456789");
+        $result = $this->words->PostChangeDocumentProtection($name="Test.docx", $filename = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostDocumentExecuteMailMerge()
+    {       
+        $file = getcwd() . '/Data/Input/SampleMailMergeTemplateData.xml';
+        $result = $this->words->PostDocumentExecuteMailMerge($name="SampleMailMergeTemplate.docx", $withRegions='false', $mailMergeDataFile = null, $cleanup = null, $filename = null, $storage = null, $folder = null, $useWholeParagraphAsRegion = null, $file);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostDocumentParagraphRunFont()
+    {       
+        $body = '{
+                    "Font:" {
+                        "Name": "Calibri"
+                    }
+                  }';
+        $result = $this->words->PostDocumentParagraphRunFont($name="Test.docx", $index="0", $runIndex="0", $storage = null, $folder = null, $filename = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostDocumentSaveAs()
+    {       
+        $body = '{
+                    "SaveFormat": "pdf",
+                    "FileName": "Test.pdf",
+                    "ImageCompression": "Jpeg",
+                    "JpegQuality": 70,
+                    "TextCompression": "Flate"
+                 }';
+
+        $response = $this->words->PostDocumentSaveAs($name="Test.docx", $storage = null, $folder = null, $body);
+        $result = json_decode($response);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostFormField()
+    {
+        $body = '{
+              "NodeId": "0.0.0",
+              "Name": "BirthDate",
+              "Enabled": true,
+              "StatusText": "Enter your date of birth.",
+              "OwnStatus": true,
+              "HelpText": "Enter your date of birth.",
+              "OwnHelp": true,
+              "CalculateOnExit": false,
+              "EntryMacro": "",
+              "ExitMacro": "",
+              "TextInputFormat": "yyyy-MM-dd",
+              "TextInputType": "Date",
+              "TextInputDefault": "",
+              "MaxLength": 0,
+              "link": {
+                "Href": "FormFilled.docx/sections/0/paragraphs/0/formfields/0",
+                "Rel": "self",
+                "Type": null,
+                "Title": null
+              }
+            }';
+        $result = $this->words->PostFormField($name="FormFilled.docx", $sectionIndex="0", $paragraphIndex="0", $formfieldIndex="0", $destFileName = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostInsertDocumentWatermarkImage()
+    {
+        $file = getcwd() . '/Data/Input/watermark.png';
+        $result = $this->words->PostInsertDocumentWatermarkImage($name="Test.docx", $filename = null, $rotationAngle = null, $image = null, $storage = null, $folder = null, $file);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostInsertDocumentWatermarkText()
+    {
+        $body = array('Text' => 'Watermark text here', 'RotationAngle' => '45.0');
+        $result = $this->words->PostInsertDocumentWatermarkText($name="Test.docx", $filename = null, $text = null, $rotationAngle = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostInsertPageNumbers()
+    {
+        $alignment = "right";
+        $format = "{PAGE} of {NUMPAGES}";
+        $isTop = True;
+        $setPageNumberOnFirstPage = True;
+        $body = array('Format'=>$format, 'Alignment'=>$alignment,
+		'IsTop'=>$isTop, 'SetPageNumberOnFirstPage'=>$setPageNumberOnFirstPage);
+        
+        $result = $this->words->PostInsertPageNumbers($name="Test.docx", $filename = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostLoadWebDocument()
+    {
+        $xml = <<<XML
+          <LoadWebDocumentData>
+             <LoadingDocumentUrl>http://google.com</LoadingDocumentUrl>
+             <DocSaveOptionsData>
+             <SaveFormat>doc</SaveFormat>
+             <FileName>google1.doc</FileName>
+             </DocSaveOptionsData>
+          </LoadWebDocumentData>
+XML;
+        $strXML = simplexml_load_string($xml);
+        $result = $this->words->PostLoadWebDocument($strXML->asXML());
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostReplaceText()
+    {
+        $oldValue = "sample";
+        $newValue = "demo";
+        $isMatchCase = False;
+        $isMatchWholeWord = True;
+        $body = array('OldValue'=>$oldValue, 'NewValue'=>$newValue, 
+		'IsMatchCase'=>$isMatchCase, 'IsMatchWholeWord'=>$isMatchWholeWord);
+        
+        $result = $this->words->PostReplaceText($name="Test.docx", $filename = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostRunTask()
+    {
+        $result = $this->words->PostRunTask();
+        print_r($result);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostSplitDocument()
+    {
+        $result = $this->words->PostSplitDocument($name="Test.docx", $format = "pdf", $from = null, $to = null, $zipOutput = null, $storage = null, $folder = null);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostUpdateDocumentBookmark()
+    {
+        $body = array("Text" => "Update bookmark text");
+        $result = $this->words->PostUpdateDocumentBookmark($name="Test.docx", $bookmarkName="_GoBack", $filename = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPostUpdateDocumentFields()
+    {
+        $result = $this->words->PostUpdateDocumentFields($name="Test.docx", $filename = null, $storage = null, $folder = null);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPutConvertDocument()
+    {
+        $file = getcwd() . '/Data/Input/FormFilled.docx';
+        $result = $this->words->PutConvertDocument($format = "pdf", $outPath = null, $file);
+        $fh = fopen(getcwd(). '/Data/Output/FormFilled.pdf', 'w');
+        fwrite($fh, $result);
+        fclose($fh);
+        $this->assertFileExists(getcwd(). '/Data/Output/FormFilled.pdf');
+    }
+    
+    public function testPutDocumentFieldNames()
+    {
+        $result = $this->words->PutDocumentFieldNames($useNonMergeFields = null);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPutDocumentSaveAsTiff()
+    {
+        $body = '{                    
+                    "TiffCompression": "Ccitt4",
+                    "PageCount": 1,
+                    "PixelFormat": "Format16BppRgb555",
+                    "PaperColor": "Red",
+                    "Resolution": 200,
+                    "Scale": 1.5,
+                    "ImageBrightness": 0.5,
+                    "ImageColorMode": "None",
+                    "ImageContrast": 0.9,
+                    "UseHighQualityRendering": true,
+                    "UseAntiAliasing": true,
+                    "PageIndex": 0,                    
+                    "NumeralFormat": "EasternArabicIndic",
+                    "SaveFormat": "tiff",
+                    "FileName": "MyDocument1.tiff"
+                  }';
+        $result = $this->words->PutDocumentSaveAsTiff($name="Test.docx", $resultFile = null, $useAntiAliasing = null, $useHighQualityRendering = null, $imageBrightness = null, $imageColorMode = null, $imageContrast = null, $numeralFormat = null, $pageCount = null, $pageIndex = null, $paperColor = null, $pixelFormat = null, $resolution = null, $scale = null, $tiffCompression = null, $dmlRenderingMode = null, $dmlEffectsRenderingMode = null, $tiffBinarizationMethod = null, $storage = null, $folder = null, $zipOutput = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPutExecuteMailMergeOnline()
+    {
+        $data['file2'] = new CurlFile(getcwd() . '/Data/Input/SampleMailMergeTemplate.docx', 'application/msword');
+        $data['file1'] = new CurlFile(getcwd() . '/Data/Input/SampleMailMergeTemplateData.xml', 'text/xml');
+        $result = $this->words->PutExecuteMailMergeOnline($withRegions='false', $cleanup = null, $data);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPutExecuteTemplateOnline()
+    {
+        $file = getcwd() . '/Data/Input/SampleMailMergeTemplateData.xml';
+        $result = $this->words->PutExecuteTemplateOnline($cleanup = null, $useWholeParagraphAsRegion = null, $withRegions = null, $file);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPutFormField()
+    {
+        $body = '{
+              "NodeId": "0.0.0",
+              "Name": "StartDate",
+              "Enabled": true,
+              "StatusText": "Enter your date of start.",
+              "OwnStatus": true,
+              "HelpText": "Enter your date of start.",
+              "OwnHelp": true,
+              "CalculateOnExit": false,
+              "EntryMacro": "",
+              "ExitMacro": "",
+              "TextInputFormat": "yyyy-MM-dd",
+              "TextInputType": "Date",
+              "TextInputDefault": "",
+              "MaxLength": 0,
+              "link": {
+                "Href": "FormFilled.docx/sections/0/paragraphs/0/formfields/0",
+                "Rel": "self",
+                "Type": null,
+                "Title": null
+              }
+            }';
+        $result = $this->words->PutFormField($name="FormFilled.docx", $sectionIndex="0", $paragraphIndex="0", $insertBeforeNode = null, $destFileName = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPutProtectDocument()
+    {
+        $password = "123456";
+        $protectionType = "AllowOnlyComments";
+        $body = array('Password' => $password, 'ProtectionType' => $protectionType);
+        
+        $result = $this->words->PutProtectDocument($name="Test.docx", $filename = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testPutUpdateDocumentProperty()
+    {
+        $body = array('Value' => 'John');   
+        $result = $this->words->PutUpdateDocumentProperty($name="Test.docx", $propertyName="Author", $filename = null, $storage = null, $folder = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testRejectAllRevisions()
+    {
+        $result = $this->words->RejectAllRevisions($name="Test.docx", $filename = null, $storage = null, $folder = null);
+        $this->assertEquals(200, $result->Code);
+    }
+    
+    public function testUpdateSectionPageSetup()
+    {
+        $body = '{
+                    "PageSetup": {
+                        "LeftMargin":99,
+                        "Orientation": "Landscape",
+                        "PaperSize": "A5"
+                    }
+                 }';
+        $result = $this->words->UpdateSectionPageSetup($name="Test.docx", $sectionIndex="0", $storage = null, $folder = null, $filename = null, $body);
+        $this->assertEquals(200, $result->Code);
+    }
                          
 }    
