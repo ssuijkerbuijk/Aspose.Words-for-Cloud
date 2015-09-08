ce86bffafacf539285bd0031e77fb6c707c91437
Farooq Sheikh <farooq.sheikh@aspose.com>
2015-09-08 17:05:30 +0500

    Update setup.py

--- SDKs/Aspose.Words_Cloud_SDK_for_Python/setup.py
+++ SDKs/Aspose.Words_Cloud_SDK_for_Python/setup.py
@@ -5,11 +5,14 @@ from setuptools import setup, find_packages
 setup(
     name = 'asposewordscloud',
     packages = find_packages(),
-    version = '1.0',
+    version = '1.0.0',
     description = 'Aspose.Words Cloud SDK for Python allows you to use Aspose.Words APIs in your Python applications',
     author='Farooq Sheikh',
     author_email='farooq.sheikh@aspose.com',
-    url='',
+    url='https://github.com/asposewords/Aspose_Words_Cloud/tree/master/SDKs/Aspose.Words_Cloud_SDK_for_Python',
+    install_requires=[
+        'asposestoragecloud',
+    ],
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
