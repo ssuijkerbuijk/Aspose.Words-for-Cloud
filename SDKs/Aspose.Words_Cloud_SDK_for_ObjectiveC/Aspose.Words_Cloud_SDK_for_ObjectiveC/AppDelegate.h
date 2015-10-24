188f867370d8cebd03f7ce4f5d1fe7860c61d7bd
Muhammad Sohail Ismail <msohailismail@gmail.com>
2015-10-24 12:22:28 +0500

    Added Aspose.Words SDK for Objective C

---
+++ SDKs/Aspose.Words_Cloud_SDK_for_ObjectiveC/Aspose.Words_Cloud_SDK_for_ObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Aspose.Words_Cloud_SDK_for_ObjectiveC
+//
+//  Created by Muhammad Sohail Ismail on 24/10/2015.
+//  Copyright © 2015 Aspose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
