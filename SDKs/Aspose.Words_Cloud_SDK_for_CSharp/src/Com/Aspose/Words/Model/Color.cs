bb4f21be7f7476933c91288efed8ce7bcf0345db
imranwar <imranwar@gmail.com>
2015-11-28 00:04:06 +0500

    C# SDK Version 1.0.0

---
+++ SDKs/Aspose.Words_Cloud_SDK_for_CSharp/src/Com/Aspose/Words/Model/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Aspose.Words.Model
+{
+    public class Color
+    {
+        public string Web {set; get;}
+        public int Alpha { set; get; }
+    }
+}
---
+++ SDKs/Aspose.Words_Cloud_SDK_for_CSharp/src/Com/Aspose/Words/Model/LoadWebDocumentData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Aspose.Words.Model
+{
+    public class LoadWebDocumentData
+    {
+        public string LoadingDocumentUrl { set; get; }
+        public Com.Aspose.Words.Model.SaveOptionsData SaveOptions { set; get; }
+    }
+}
