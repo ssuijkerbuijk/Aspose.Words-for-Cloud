19d72037c2ae28364f70aae6014f9d55d7714d31
Evgeniy Ivanov <ivanov.john@gmail.com>
2017-09-20 10:55:10 +0700

    fix script

--- SDKs/codegen/generateSDK.bat
+++ SDKs/codegen/generateSDK.bat
@@ -1,4 +1,4 @@
-del /S /Q "c:\tmp"
+del /S /Q "c:\tmp\csharp"
 java -jar SDKs\codegen\swagger-codegen-cli-2.3.0.jar generate -i asposeforcloud_word.json -l csharp -t SDKs\codegen\Templates\csharp -o c:/tmp/csharp/ -c SDKs\codegen\config.json 
 
 del /S /Q "SDKs\Aspose.Words-Cloud-SDK-for-.NET\Aspose.Words.Cloud.Sdk\Model"
