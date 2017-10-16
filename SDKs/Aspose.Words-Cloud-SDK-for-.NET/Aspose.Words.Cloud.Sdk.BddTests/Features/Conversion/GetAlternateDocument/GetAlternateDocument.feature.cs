69b6c398c98b4f4ef26d086453944f380806d913
EkimovYN <slawachke@gmail.com>
2017-10-16 14:24:27 +0700

    WORDSCLOUD223 - mailmerge and tempalte execute tests

--- SDKs/Aspose.Words-Cloud-SDK-for-.NET/Aspose.Words.Cloud.Sdk.BddTests/Features/Conversion/GetAlternateDocument/GetAlternateDocument.feature.cs
+++ SDKs/Aspose.Words-Cloud-SDK-for-.NET/Aspose.Words.Cloud.Sdk.BddTests/Features/Conversion/GetAlternateDocument/GetAlternateDocument.feature.cs
@@ -180,6 +180,124 @@ this.ScenarioSetup(scenarioInfo);
         {
             this.ConversionOfStoredDocument("TableDocument.doc", "htmlfixed", ((string[])(null)));
         }
+        
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage(string docName, string destFormat, string outPath, string[] exampleTags)
+        {
+            string[] @__tags = new string[] {
+                    "ConversionOfStoredDoc"};
+            if ((exampleTags != null))
+            {
+                @__tags = System.Linq.Enumerable.ToArray(System.Linq.Enumerable.Concat(@__tags, exampleTags));
+            }
+            TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Conversion of stored document and save to storage", @__tags);
+#line 26
+this.ScenarioSetup(scenarioInfo);
+#line 27
+ testRunner.Given(string.Format("I have uploaded document with name {0} to storage", docName), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Given ");
+#line 28
+ testRunner.And(string.Format("I have specified format {0} document to be converted", destFormat), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 29
+ testRunner.And(string.Format("I have specified output path {0}", outPath), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 30
+ testRunner.And(string.Format("I have specified document name {0} in URL", docName), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+#line 31
+ testRunner.When("I execute conversion from storage (GET document with format)", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 32
+ testRunner.Then("converted document downloads from storage", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
+#line hidden
+            this.ScenarioCleanup();
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Conversion of stored document and save to storage")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Stored document conversion with return result in response")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute("ConversionOfStoredDoc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("VariantName", "Variant 0")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DocName", "TableDocument.doc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DestFormat", "pdf")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:OutPath", "out/getalternate/TableDocument.pdf")]
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage_Variant0()
+        {
+            this.ConversionOfStoredDocumentAndSaveToStorage("TableDocument.doc", "pdf", "out/getalternate/TableDocument.pdf", ((string[])(null)));
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Conversion of stored document and save to storage")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Stored document conversion with return result in response")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute("ConversionOfStoredDoc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("VariantName", "Variant 1")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DocName", "test_doc.docx")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DestFormat", "pdf")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:OutPath", "out/getalternate/test_doc.pdf")]
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage_Variant1()
+        {
+            this.ConversionOfStoredDocumentAndSaveToStorage("test_doc.docx", "pdf", "out/getalternate/test_doc.pdf", ((string[])(null)));
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Conversion of stored document and save to storage")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Stored document conversion with return result in response")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute("ConversionOfStoredDoc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("VariantName", "Variant 2")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DocName", "test_doc.docx")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DestFormat", "png")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:OutPath", "out/getalternate/test_doc.png")]
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage_Variant2()
+        {
+            this.ConversionOfStoredDocumentAndSaveToStorage("test_doc.docx", "png", "out/getalternate/test_doc.png", ((string[])(null)));
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Conversion of stored document and save to storage")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Stored document conversion with return result in response")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute("ConversionOfStoredDoc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("VariantName", "Variant 3")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DocName", "test_doc.docx")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DestFormat", "html")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:OutPath", "out/getalternate/test_doc.html")]
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage_Variant3()
+        {
+            this.ConversionOfStoredDocumentAndSaveToStorage("test_doc.docx", "html", "out/getalternate/test_doc.html", ((string[])(null)));
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Conversion of stored document and save to storage")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Stored document conversion with return result in response")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute("ConversionOfStoredDoc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("VariantName", "Variant 4")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DocName", "TableDocument.doc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DestFormat", "html")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:OutPath", "out/getalternate/TableDocument.html")]
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage_Variant4()
+        {
+            this.ConversionOfStoredDocumentAndSaveToStorage("TableDocument.doc", "html", "out/getalternate/TableDocument.html", ((string[])(null)));
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Conversion of stored document and save to storage")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Stored document conversion with return result in response")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute("ConversionOfStoredDoc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("VariantName", "Variant 5")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DocName", "TableDocument.doc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DestFormat", "txt")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:OutPath", "out/getalternate/TableDocument.txt")]
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage_Variant5()
+        {
+            this.ConversionOfStoredDocumentAndSaveToStorage("TableDocument.doc", "txt", "out/getalternate/TableDocument.txt", ((string[])(null)));
+        }
+        
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute("Conversion of stored document and save to storage")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Stored document conversion with return result in response")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryAttribute("ConversionOfStoredDoc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("VariantName", "Variant 6")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DocName", "TableDocument.doc")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:DestFormat", "htmlfixed")]
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("Parameter:OutPath", "out/getalternate/TableDocument.htmlfixed")]
+        public virtual void ConversionOfStoredDocumentAndSaveToStorage_Variant6()
+        {
+            this.ConversionOfStoredDocumentAndSaveToStorage("TableDocument.doc", "htmlfixed", "out/getalternate/TableDocument.htmlfixed", ((string[])(null)));
+        }
     }
 }
 #pragma warning restore
--- SDKs/Aspose.Words-Cloud-SDK-for-.NET/Aspose.Words.Cloud.Sdk.BddTests/Features/Conversion/SaveAs/SaveAsSteps.cs
+++ SDKs/Aspose.Words-Cloud-SDK-for-.NET/Aspose.Words.Cloud.Sdk.BddTests/Features/Conversion/SaveAs/SaveAsSteps.cs
@@ -1,6 +1,32 @@
-﻿namespace Aspose.Words.Cloud.Sdk.BddTests.Features.Conversion.SaveAs
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Aspose" file="SaveAsSteps.cs">
+//   Copyright (c) 2016 Aspose.Words for Cloud
+// </copyright>
+// <summary>
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Aspose.Words.Cloud.Sdk.BddTests.Features.Conversion.SaveAs
 {
     using System.IO;
+    using System.Xml;
 
     using Aspose.Words.Cloud.Sdk.BddTests.Base.Context;
     using Aspose.Words.Cloud.Sdk.Model;
@@ -42,7 +68,6 @@
             }
         }
 
-
         /// <summary>
         /// Initialize context before each scenario.
         /// </summary>
@@ -72,8 +97,6 @@
             this.Request.SaveOptionsData.FileName = destFileName;
         }
 
-
-
         /// <summary>
         /// Executes conversion
         /// </summary>
@@ -91,11 +114,8 @@
         public void ThenSymbolsAreEncodedProperly()
         {
             this.File.Position = 0;
-            var formatInfo = FileFormatUtil.DetectFileFormat(this.File);
-            if (formatInfo.LoadFormat != LoadFormat.Unknown && formatInfo.Encoding != null)
-            {
-                Assert.IsTrue(formatInfo.Encoding.BodyName == this.Request.LoadEncoding);
-            }
+            var doc = new Words.Document(this.File);
+            Assert.IsTrue(doc.GetText().Contains("строка"), "Wrong conversion");
         }        
     }
 }
