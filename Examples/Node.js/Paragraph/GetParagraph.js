ee7af48a124ca9f36856d3d2a0083a3b36d29098
mateen sajjad <mateen.sajjad@aspose.com>
2018-03-07 01:06:12 +0500

    Updated NodeJS Examples

--- Examples/Node.js/Paragraph/GetParagraph.js
+++ Examples/Node.js/Paragraph/GetParagraph.js
@@ -1,43 +1,16 @@
-// ExStart:1
-var fs = require('fs');
-var assert = require('assert');
-var StorageApi = require('asposestoragecloud');
-var WordsApi = require('asposewordscloud');
-var configProps = require('../Config/config.json');
-var data_path = '../../../Data/';
-
-var AppSID = configProps.app_sid;
-var AppKey = configProps.api_key;
-var config = {'appSid':AppSID,'apiKey':AppKey , 'debug' : true};
-
-// Instantiate Aspose Storage API SDK
-var storageApi = new StorageApi(config);
-// Instantiate Aspose Words API SDK
-var wordsApi = new WordsApi(config);
+const { WordsApi,GetDocumentParagraphRequest} = require("asposewordscloud");
+wordsApi = new WordsApi('78946fb4-3bd4-4d3e-b309-f9e2ff9ac6f9', 'b125f13bf6b76ed81ee990142d841195');
 
 // Set input file name
-var name = "SampleWordDocument.docx";
-var index = 1;
+var fileName = "SampleWordDocument.docx";
+const request = new GetDocumentParagraphRequest();
+request.name = fileName;
+request.index = 0;
 
-try {
-// Upload source file to aspose cloud storage
-storageApi.PutCreate(name, versionId=null, storage=null, file= data_path + name , function(responseMessage) {
 
-	assert.equal(responseMessage.status, 'OK');
+wordsApi.getDocumentParagraph(request).then((result) => {
+        console.log(result);
+}).catch(function(err) {
+    console.log(err);
+});
 
-	// Invoke Aspose.Words Cloud SDK API to get a specific paragraph from a word document
-	wordsApi.GetDocumentParagraph(name, index, null, null, function(responseMessage) {
-			assert.equal(responseMessage.status, 'OK');
-			var docParagraph = responseMessage.body.Paragraph;
-			
-			// Display document paragraph info
-			console.log("NoteId : " + docParagraph.NodeId);
-			console.log("Link : " + docParagraph.link.Href);
-			});
-	});
-
-}catch (e) {
-  console.log("exception in example");
-  console.log(e);
-}
-//ExEnd:1
