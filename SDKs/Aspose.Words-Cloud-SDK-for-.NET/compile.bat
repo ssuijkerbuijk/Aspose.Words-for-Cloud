f26fbf985bac6192ec32923987826e39cafc9c8e
SOHAIL <msohailismail@gmail.com>
2018-03-22 23:47:29 +0500

    Deleted .NET, PHP and Node.js deprecated SDKs

--- SDKs/Aspose.Words-Cloud-SDK-for-.NET/compile.bat
+++
@@ -1,2 +0,0 @@
-SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
-%CSCPATH%\csc /reference:bin/Newtonsoft.Json.dll /target:library /out:bin/Com.Aspose.Words.dll /recurse:src\*.cs /doc:bin/Com.Aspose.Words.xml
--- SDKs/Aspose.Words-Cloud-SDK-for-PHP/src/Aspose/Words/ApiException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Aspose\Words;
-
-class ApiException extends \Exception
-{
-
-}
